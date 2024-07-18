c449a410ed0609bd324e1eabd119ae833cbcc51e
Frank Mwito <Mfranklyne039@gmail.com>
2024-07-18 15:33:55 +0300

    Create a client

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,3 +19,9 @@ tasks.test {
 kotlin {
     jvmToolchain(21)
 }
+val ktor_version: String by project
+
+dependencies {
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 package org.example
 
-fun main() {
-    println("Hello World!")
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+
+suspend fun main() {
+    val client = HttpClient(CIO)
+    val response: HttpResponse = client.get("http://Ktor.io")
+    println(response.status)
+    client.close()
 }
