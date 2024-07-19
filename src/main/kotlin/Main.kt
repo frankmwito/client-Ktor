d6601aff70aae6dafc86fe367c21495d4f05641c
Frank Mwito <Mfranklyne039@gmail.com>
2024-07-19 11:32:14 +0300

    configure an engine

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,8 +6,11 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 
 suspend fun main() {
-    val client = HttpClient(CIO){
-        expectSuccess = true
+    val client = HttpClient(CIO){ // configure a client
+        expectSuccess = true //response validation
+        engine {
+         //configure an engine
+        }
     }
     val response: HttpResponse = client.get("http://Ktor.io")
     println(response.status)
